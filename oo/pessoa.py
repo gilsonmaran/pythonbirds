c82314fa392f771083d983f9d7f80460a0a97fb2
= <--replace>
2020-03-05 23:46:36 -0300

    Adicionado os atributos na classe Pessoa. Adicionado a função Main para testes.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,18 @@
 class Pessoa:
+    def __init__(self, *filhos, nome = None, idade = None):
+        self.filhos = list(filhos)
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
-        return 'Olá'
+        return 'Olá ' + self.nome
+
+if __name__ == '__main__':
+    luciano = Pessoa(nome = 'Luciano')
+    gilson = Pessoa(luciano, nome = 'Gilson')
+    print(luciano.cumprimentar())
+    print(gilson.cumprimentar())
+
+    for filho in gilson.filhos:
+        print(filho.nome)
+
